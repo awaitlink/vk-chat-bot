a2a5b5f53fa064dbbe991bbbe880788205db5aaf
Artem Varaksa <aymfst@gmail.com>
2017-12-02 20:09:09 +0300

    Initial commit

---
+++ example/main.js
@@ -0,0 +1,34 @@
+const bot = require('vk-chat-bot');
+
+// Server port
+const port = process.env.PORT;
+
+var params = {
+  vk_api_key: process.env.VK_API_KEY,
+
+  // Confirmation parameters, can be found in group Callback API settings
+  confirmation_token: process.env.CONFIRMATION_TOKEN,
+  group_id: process.env.GROUP_ID,
+
+  // Secret key, set in group Callback API settings
+  secret: process.env.SECRET,
+
+  // Command prefix, optional
+  cmd_prefix: "/"
+}
+
+bot.init(params);
+
+bot.event("message_allow", (uid) => {
+  return "Hello, thanks for allowing us to send you messages.";
+});
+
+bot.on("test", (msg) => {
+  return "Test success! Your message was: \"" + msg + "\".";
+});
+
+bot.onlike("(hi|hello|hey)", (msg) => {
+  return "Hello, I am a bot.";
+});
+
+bot.start(port);
---
+++ src/vk-chat-bot.js
@@ -0,0 +1,173 @@
+const app = require('express')();
+const https = require('https');
+const request = require('request');
+const bodyParser = require('body-parser');
+
+var groupId, confirmationToken, secret, vkApiKey;
+var cmdPrefix;
+
+var initialised = false;
+
+var commandHandlers = [];
+var regexHandlers = [];
+var eventHandlers = [];
+var possibleEvents = ["message_allow", "message_deny"];
+
+// Initialise the bot
+exports.init = function (params) {
+  groupId = params.group_id;
+  confirmationToken = params.confirmation_token;
+  secret = params.secret;
+  vkApiKey = params.vk_api_key;
+  cmdPrefix = params.cmd_prefix;
+
+  if (groupId != null && confirmationToken != null && secret != null && vkApiKey != null) {
+    initialised = true;
+  } else {
+    console.log('[!] Bad init parameters.');
+    console.log('[!] Terminating.');
+    process.exit(1);
+  }
+};
+
+// On exact command with prefix
+exports.on = function (command, callback) {
+  commandHandlers.push({
+    command: command,
+    callback: callback
+  });
+}
+
+// On matching regex
+exports.onlike = function (regex, callback) {
+  regexHandlers.push({
+    regex: regex,
+    callback: callback
+  });
+}
+
+// For special events
+exports.event = function (e, callback) {
+  if (!possibleEvents.includes(e)) {
+    console.log('[!] Tried to register a handler for an unsupported event type: ', e);
+    console.log('[!] Terminating.');
+    process.exit(1);
+  }
+
+  eventHandlers.push({
+    event: e,
+    callback: callback
+  });
+}
+
+// Start the bot
+exports.start = function (port) {
+  if (!initialised) {
+    console.log('[!] Please initialise before starting using init(params).');
+    console.log('[!] Terminating.');
+    process.exit(1);
+  }
+
+  app.use(bodyParser.json());
+
+  app.get('/', (req, res) => {
+    res.status(400).send('Only POST allowed.');
+    console.log('[>] GET request.');
+  })
+
+  app.post('/', (req, res) => {
+    body = req.body;
+
+    if (body.type === "confirmation" && body.group_id == groupId) {
+        res.status(200).send(confirmationToken);
+    } else if (body.secret === secret) {
+        res.status(200).send('ok');
+        parseRequest(body);
+    } else {
+        res.status(400).send('Invalid secret key.');
+        console.log('[>] Request with an invalid secret key.');
+    }
+  })
+
+  app.listen(port, (err) => {
+    if (err) return console.log('[!] Error: ', err);
+    console.log(`[i] Server is listening on port ${port}.`);
+  })
+};
+
+// Parse Callback API's message
+function parseRequest(body) {
+  if (body.type === "message_new") {
+    uid = body.object.user_id;
+    msg = body.object.body;
+    console.log('[>] New message from user: ', uid);
+    handleMessage(uid, msg);
+  } else {
+    uid = body.object.user_id;
+    console.log('[>] User allowed messages: ', uid);
+    handleEvent(uid, body.type)
+  }
+}
+
+// Handle message_new
+function handleMessage(uid, msg) {
+  msg = msg.toLocaleLowerCase();
+  var command = msg.split(" ")[0].replace(cmdPrefix, "");
+
+  // See if there is a matching command
+  for (var i = 0; i < commandHandlers.length; i++) {
+    handler = commandHandlers[i];
+    if (handler.command === command) {
+      msg_content = msg.replace(cmdPrefix + command, "");
+
+      var answer = handler.callback(msg_content);
+      if (answer != null) {
+        send(uid, answer)
+      }
+
+      return;
+    }
+  }
+
+  // If not, try to use a regex handler
+  for (var i = 0; i < regexHandlers.length; i++) {
+    handler = regexHandlers[i];
+    if (handler.regex.test(msg)) {
+      var answer = handler.callback(msg);
+      if (answer != null) {
+        send(uid, answer)
+      }
+
+      return;
+    }
+  }
+
+  // If not, log
+  console.log("[!] Don't know how to respond to: ", msg);
+}
+
+// Handle a special event
+function handleEvent(uid, e) {
+  if (!possibleEvents.includes(e)) {
+    console.log('[!] Received an unsupported event type: ', body.type);
+  } else {
+    for (var i = 0; i < eventHandlers.length; i++) {
+      handler = eventHandlers[i];
+      if (handler.event === e) {
+        var answer = handler.callback(uid);
+        if (answer != null) {
+          send(uid, answer)
+        }
+      }
+    }
+  }
+}
+
+// Send a message to user by his id
+function send(uid, msg) {
+  var url = `https://api.vk.com/method/messages.send?user_id=${uid}&message=${encodeURIComponent(msg)}&access_token=${vkApiKey}`;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) console.log("[<] Message sent to user: ", uid);
+    if (error) console.log('[!] Error occured when sending a message: ', error);
+  })
+};
