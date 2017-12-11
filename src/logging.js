0b3f57f68d2528867a281ed53db8d334d7eb84af
Artem Varaksa <aymfst@gmail.com>
2017-12-11 19:56:26 +0300

    Fix mistake in logging

--- src/logging.js
+++ src/logging.js
@@ -11,11 +11,11 @@ exports.log = function(type, text) {
 };
 
 exports.terminate = function() {
-  log(exports.type.error, 'Terminating.');
+  exports.log(exports.type.error, 'Terminating.');
   process.exit(1);
 };
 
 exports.badParams = function(functionName) {
-  log(exports.type.error, 'Bad parameters for function ' + functionName + '().');
-  terminate();
+  exports.log(exports.type.error, 'Bad parameters for function ' + functionName + '().');
+  exports.terminate();
 };
