3c40688893e515416a0de4c3390022b8caccdecc
Artem Varaksa <aymfst@gmail.com>
2018-11-05 12:05:27 +0300

    ...

--- .travis/deploy_gh_pages.sh
+++ .travis/deploy_gh_pages.sh
@@ -2,9 +2,9 @@
 set -e
 
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_7b49c23863d8_key -iv $encrypted_7b49c23863d8_iv -in key.enc -out ~/travis/key -d
-chmod 600 ~/travis/key
-ssh-add ~/travis/key
+openssl aes-256-cbc -K $encrypted_7b49c23863d8_key -iv $encrypted_7b49c23863d8_iv -in key.enc -out ~/key -d
+chmod 600 ~/key
+ssh-add ~/key
 
 yarn add -D gh-pages
 ./node_modules/.bin/gh-pages -d docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
