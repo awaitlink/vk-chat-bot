2bf393040748745da8be5454ab09b4fcaef1d308
Artem Varaksa <aymfst@gmail.com>
2018-11-05 12:19:59 +0300

    . . .

--- .travis/deploy_gh_pages.sh
+++ .travis/deploy_gh_pages.sh
@@ -6,5 +6,6 @@ openssl aes-256-cbc -K $encrypted_7b49c23863d8_key -iv $encrypted_7b49c23863d8_i
 chmod 600 ~/key
 ssh-add ~/key
 
-yarn global add -D gh-pages --ignore-scripts --global-folder .
+yarn config set prefix .
+yarn global add gh-pages --ignore-scripts
 ./gh-pages -d docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
