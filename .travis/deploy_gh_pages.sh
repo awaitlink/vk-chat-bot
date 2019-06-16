8fdb291bf3fd6077447611441bcfe74248d03a52
Artem Varaksa <aymfst@gmail.com>
2019-06-16 19:41:44 +0300

    Add `gh-pages` as `devDependency`.

--- .travis/deploy_gh_pages.sh
+++ .travis/deploy_gh_pages.sh
@@ -6,5 +6,4 @@ openssl aes-256-cbc -K $encrypted_7b49c23863d8_key -iv $encrypted_7b49c23863d8_i
 chmod 600 ~/key
 ssh-add ~/key
 
-npm i -g -C . gh-pages
-bin/gh-pages -t -d ../docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
+npx gh-pages -t -d ../docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
