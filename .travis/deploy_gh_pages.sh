aacb2e4da5ca419e8981950df59081b42d7b960d
Artem Varaksa <aymfst@gmail.com>
2019-08-16 10:41:36 +0300

    GitHub CI

--- .travis/deploy_gh_pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_7b49c23863d8_key -iv $encrypted_7b49c23863d8_iv -in key.enc -out ~/key -d
-chmod 600 ~/key
-ssh-add ~/key
-
-npx gh-pages -t -d ../docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
